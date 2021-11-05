85c4ef45fb426855aef279b975b5899d91351c5f
dmanojlo <dragojlo@net.hr>
2021-11-05 16:24:03 +0100

    ini

--- furniture_scraper/settings.py
+++ furniture_scraper/settings.py
@@ -15,7 +15,8 @@ NEWSPIDER_MODULE = 'furniture_scraper.spiders'
 FEED_EXPORT_ENCODING = 'utf-8'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-USER_AGENT = 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
+USER_AGENT = 'Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36'
+#HTTPERROR_ALLOWED_CODES  = [404]
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
@@ -26,7 +27,7 @@ ROBOTSTXT_OBEY = False
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 2
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
--- furniture_scraper/spiders/emezzeta_spider.py
+++ furniture_scraper/spiders/emezzeta_spider.py
@@ -26,33 +26,43 @@ class EmezzetaSpider(scrapy.Spider):
             for desne in response.css('div.product-item-info'):
                 if desne.css('span.clearance-icon::text') or desne.css('span.action-icon::text'):
                     yield{'item_name': desne.css('a.product-item-link::text').get().strip(),
-                          'regular_price': desne.css('span.old-price .price::text').get(),
+                          'old_price': desne.css('span.old-price .price::text').get(),
                           'discount_price': desne.css('span.price::text').get(),
+                          'img_link': desne.css(".product-image-photo::attr(data-bg-src)").get(),
+                          'page_link': desne.css('a::attr(href)').get()
                         }
 
                 else:
                     yield{'item_name': desne.css('a.product-item-link::text').get().strip(),
-                          'regular_price': desne.css('span.price::text').get(),
+                          'old_price': desne.css('span.price::text').get(),
                           'discount_price': desne.css('span.loyalty-price .price::text').get(),
+                          'img_link': desne.css(".product-image-photo::attr(data-bg-src)").get(),
+                          'page_link': desne.css('a::attr(href)').get()
                         }
 
         if 'kuhinja' in response.url:
             for kitchen in response.css('div.product-item-info'):
                 if kitchen.css('span.clearance-icon::text') or kitchen.css('span.action-icon::text'):
                     yield{'item_name': kitchen.css('a.product-item-link::text').get().strip(),
-                          'regular_price': kitchen.css('span.old-price .price::text').get(),
+                          'old_price': kitchen.css('span.old-price .price::text').get(),
                           'discount_price': kitchen.css('span.price::text').get(),
+                          'img_link': kitchen.css(".product-image-photo::attr(data-bg-src)").get(),
+                          'page_link': kitchen.css('a::attr(href)').get()
                         }
 
                 else:
                     yield{'item_name': kitchen.css('a.product-item-link::text').get().strip(),
-                          'regular_price': kitchen.css('span.price::text').get(),
+                          'old_price': kitchen.css('span.price::text').get(),
                           'discount_price': kitchen.css('span.loyalty-price .price::text').get(),
+                          'img_link': kitchen.css(".product-image-photo::attr(data-bg-src)").get(),
+                          'page_link': kitchen.css('a::attr(href)').get()
                         }
 
 
 class MimaSpider(scrapy.Spider):
     name = 'mima'
+
+    handle_httpstatus_list = [404]
 
     start_urls = ['https://namjestaj-mima.hr/proizvodi/?searchcode=basic&searchid=3&with_qty=0&discount=1&only_available=0&categories=4398&price=0&price-e=26999&search_q=&to_page=2']
 
@@ -63,7 +73,9 @@ class MimaSpider(scrapy.Spider):
             if kutne.css('div.cp-old-price'):
                 yield{'item_name': kutne.css('h2.cp-title::text').get(),
                       'old_price': kutne.css('div.cp-old-price::text').get(),
-                      'discount_price': kutne.css('div.cp-discount-price::text').get()
+                      'discount_price': kutne.css('div.cp-discount-price::text').get(),
+                      'img_link': kutne.css("figure.cp-image img::attr(data-original)").get(),
+                      'page_link': kutne.css('a::attr(href)').get()
                      }
 
 
@@ -71,7 +83,6 @@ class LesninaSpider(scrapy.Spider):
     name = 'lesnina'
 
     start_urls = ['https://www.xxxlesnina.hr/api/graphql?operationName=search&variables=%7B%22cid%22%3Anull%2C%22encodedFhLink%22%3A%22null%22%2C%22filters%22%3A%22%5B%7B%5C%22id%5C%22%3A%5C%22categories%5C%22%2C%5C%22type%5C%22%3A%5C%22categoryFilter%5C%22%2C%5C%22values%5C%22%3A%5B%5C%22C1%5C%22%2C%5C%22C1C1%5C%22%2C%5C%22C1C1C1%5C%22%5D%7D%2C%7B%5C%22id%5C%22%3A%5C%22v_atrm_uklju_eno_u_cijenu%5C%22%2C%5C%22type%5C%22%3A%5C%22multiSelectFilter%5C%22%2C%5C%22elementId%5C%22%3A%5C%22funkcija_kreveta%5C%22%2C%5C%22values%5C%22%3A%5B%5C%22funkcija_kreveta%5C%22%5D%7D%2C%7B%5C%22id%5C%22%3A%5C%22v_eyecatcher%5C%22%2C%5C%22type%5C%22%3A%5C%22multiSelectFilter%5C%22%2C%5C%22elementId%5C%22%3A%5C%22bestprice-sale%5C%22%2C%5C%22values%5C%22%3A%5B%5C%22bestprice%5C%22%2C%5C%22sale%5C%22%5D%7D%5D%22%2C%22pagination%22%3A%22%7B%5C%22page%5C%22%3A1%2C%5C%22numberOfResults%5C%22%3A60%7D%22%2C%22sortBy%22%3A%22%5C%22popular%5C%22%22%2C%22searchTerm%22%3A%22%5C%22%5C%22%22%2C%22type%22%3Anull%7D&extensions=%7B%22persistedQuery%22%3A%7B%22version%22%3A1%2C%22sha256Hash%22%3A%22b6f527bf074c162f0fe9dd316c2d9144a159f15ac6a8d323e8ffc4da98914152%22%7D%7D']
-
 
     custom_settings = { 'FEEDS': {'lesnina_result.json': {'format': 'json', 'overwrite': True}}}
 
@@ -84,10 +95,14 @@ class LesninaSpider(scrapy.Spider):
                     yield{'item_name': search_res[i]['name'].strip(),
                           'old_price': search_res[i]['priceData']['oldPrice']['value'],
                           'discount_price': search_res[i]['priceData']['currentPrice']['value'],
+                          'img_link': 'https://media.xxxlutz.com/i/xxxlutz/' + search_res[i]['mediaData']['presentation'][0]['cdnFilename'] + '/?fmt=auto&w=420&h=315',
+                          'page_link': 'https://www.xxxlesnina.hr' + search_res[i]['url'].strip()
                         }
                 else:
                     yield{'item_name': search_res[i]['name'].strip(),
                           'low_price': search_res[i]['priceData']['currentPrice']['value'],
+                          'img_link': 'https://media.xxxlutz.com/i/xxxlutz/' + search_res[i]['mediaData']['presentation'][0]['cdnFilename'] + '/?fmt=auto&w=420&h=315',
+                          'page_link': 'https://www.xxxlesnina.hr' + search_res[i]['url'].strip()
                         }
 
 
@@ -104,6 +119,8 @@ class PrimaSpider(scrapy.Spider):
         for kutne in response.css('div.product-item-info'):
             yield{'item_name': kutne.css('a.product-item-link::text').get().strip(),
                   'action_price': kutne.css('span.price::text').get(),
+                  'img_link': kutne.css(".product-image-photo::attr(data-src)").get(),
+                  'page_link': kutne.css('a::attr(href)').get()
                  }
 
 
@@ -118,6 +135,10 @@ class HarveySpider(scrapy.Spider):
         for kutne in response.css('div.product-item-info'):
             if kutne.css('div.discount.procent'):
                 yield{'item_name': kutne.css('a.product-item-link::text').get().strip(),
+                      'old_price': kutne.css('span.old-price .price::text').get(),
+                      'discount_price': kutne.css('span.price::text').get(),
+                      'img_link': kutne.css(".product-image-photo::attr(data-static-image)").get(),
+                      'page_link': kutne.css('a::attr(href)').get()
                      }
 
         next_page = response.css('a.action.next').attrib['href']
